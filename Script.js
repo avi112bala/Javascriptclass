129f9bb38fb2191a1e7179df7433c61a1ec851b0
avi112bala <avinash20802bala@gmail.com>
2023-06-22 18:21:39 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,35 @@
+
+// A= "Hello Guys"
+// document.write(A);
+// document.write(A);
+
+
+// we do not write the variable name 
+// my name 
+// 99nme
+
+
+// VAR
+
+// var a="hello"
+// var a="my name"
+// document.write(a)
+
+
+// LET 
+
+// let b="hello "
+// let b="myname"
+// document.write(b)
+
+// let c="myname"
+// c="hello"
+// document.write(c)
+
+
+// CONST 
+
+// const b="hello"
+// const b="hi"
+// b="hh"
+// document.write(b)
